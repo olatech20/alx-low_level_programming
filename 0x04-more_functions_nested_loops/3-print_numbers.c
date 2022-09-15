6a7b433bf9c7f0055c9e6da80be32a1780283508
olatech20 <afeezdipo@gmail.com>
2022-09-15 05:49:05 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,14 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main -> a program that prints the alphabets in lowercase,
- * followed by a new line
- * Return: Always 0
- */
- int main(void)
- {
-
-	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		if (ch != 'q' && ch != 'e')
-		{
-			putchar(ch);
-		}
-		putchar(10);
-		return (0);
-	}
- }
+* print_numbers - prints the numbers, from 0 to 9,
+* followed by a new line
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
